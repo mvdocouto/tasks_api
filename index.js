c8366df16ad05c967ab3e8529b435b9a880f0624
Marcus Couto <marcus.couto@stormsec.com.br>
2018-02-12 18:11:35 -0200

    add  consign package

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import express  from "express";
+const express = require("express")
+const consign = require("consign")
+// import express  from "express";
+// import consign  from "consign";
 const httpPort = 3000;
 const app = express();
 
-app.get("/", (req, res) => res.json({server: "running"}));
+app.set("json spaces", 4)
+
+consign()
+    .include("models")
+    .then("routes")
+    .into(app);
+
+
 app.listen(httpPort, () => {
     console.log(`Estamos vivos! http://localhost:${httpPort}`);
 });
