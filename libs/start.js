23989b01a2a792b308072029422de7660e1c605f
Marcus Couto <marcus.couto@stormsec.com.br>
2018-02-14 15:18:25 -0200

    sync db before running server

--- libs/start.js
+++ libs/start.js
@@ -1,5 +1,9 @@
 module.exports = app => {
-    app.listen(app.get("port"), () =>{
-        console.log(`Estamos vivos! http://localhost:${app.get("port")}`);
+    // Sync database an table before load server.
+    app.db.sync().done( () => {
+        // running server
+        app.listen(app.get("port"), () =>{
+            console.log(`Estamos vivos! http://localhost:${app.get("port")}`);
+        });    
     });
 }
